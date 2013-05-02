5175ec3425198612c514e674fa8f26a5a4fa0d5d
Courtney <chirose@cars.com>
2013-05-02 12:57:51 -0700

    Tuple Review

--- hello.py
+++ hello.py
@@ -12,8 +12,9 @@
 
 #Python Class 3:
 
-##Python Counting
-#Arrays have starting positions at 0
+##Python Counting: Arrays have starting positions at 0
+##Python: Reads from top to bottom
+
 
 ##Variable Declaration
 person_name = "Courtney"
@@ -22,7 +23,25 @@ gender = "her"
 
 ##Type of Object: Tuple
 #Creating a variable, person, and assigning three traits to it
+#Cannot change a tuple unless make a copy of it
+#Cannot change an individual element of a tuple
 person = (person_name, favorite_number, gender)
+
+#**SHORTCUT**#
+#Assigns Variables All at Once Instead of One at a Time
+new_name, new_number, new_gender = ('Paul',16,'his')
+
+
+andrew = ('Andrew',6,'his')
+courtney = ('Courtney',21,'her')
+
+print andrew[0]
+print andrew[1]
+print andrew[2]
+
+##Reassigned the variable person to be andrew
+person = andrew
+
 ##Can access elements in an array through indexing
 #person[0] -> references person_name. 
 print person[0]
@@ -31,8 +50,8 @@ print person[2]
 
 ##Populates the {} with the variable that is above
 print "Howdy"
-print "my name is {}".format(person_name)
-print "my favorite number is {}".format(favorite_number)
+print "my name is {}".format(person[0])
+print "my favorite number is {}".format(person[1])
 
 #List all the things I have my favorite number of
 print "{} has {} books in {} collection".format(person[0],person[1],person[2])
