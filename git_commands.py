ce1725b4de813053d0a1f1d69ea0b7d5379e34ae
unknown <Chirose@CV10838.corp.classifiedventures.com>
2013-05-02 12:16:54 -0700

    Added Git Commands

---
+++ git_commands.py
@@ -0,0 +1,22 @@
+##Courtney Hirose
+##HW 3
+##Writing git commands and explaining their purpose
+
+print "git init:Creates a repository from an existing directory of files"
+print "git init"
+
+print "git status: Shows the working tree status"
+print "git status"
+
+print "git log: Shows the commit logs"
+print "git log"
+
+print "git add: Adds file contents to the index"
+print "git add <filename>"
+
+print "git diff: shows changes between commits, commit and working tree, etc."
+print "git diff A..B"
+
+print "git commit: records changes to repository"
+print "git commit -m"
+
