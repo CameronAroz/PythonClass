05653c1b66f5e9e65e791e666a78433bf347e377
Courtney <chirose@cars.com>
2013-05-08 15:52:24 -0700

    Homework 3 Answers

---
+++ homework3.py
@@ -0,0 +1,34 @@
+##Homework 3
+
+##Part A:
+#Import Library
+from sys import argv
+
+#Assign input to variable
+number1 = int(argv[1])
+number2 = int(argv[2])
+
+#Prints Summation of Arguments
+print number1 + number2
+
+##Part B:
+#1. Append/Extend/Pop/Sort/Reverse: Will not work on tuples because they cannot be changed
+#2. Length and Type functions will work because they don't change the tuple
+
+
+##Part E:
+#In order to find the number of times and item appears in a list
+
+#Declaration of the List
+num = [3,1,4,1,5,9,2,6]
+
+print "Provided the List: num = [3,1,4,1,5,9,2,6]"
+print "The Number 1 appears:", num.count(1), "times"
+
+
+
+
+
+
+
+
