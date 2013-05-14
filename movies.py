f8fb37d93145d4aba870145c557b9b74122c4f38
Courtney <chirose@cars.com>
2013-05-14 10:49:24 -0700

    Created a loop to print out different elements in a list

---
+++ movies.py
@@ -0,0 +1,17 @@
+##Write a program that adds the movies above to a list names movies using a 
+#for loop and the new "movies" list, print the name and rating of the movie
+
+
+movie_1 = ('Iron Man 3', 78)
+movie_2 = ('The Great Gatsby', 48)
+movie_3 = ('Star Trek Into Darkness', 90)
+movie_4 = ('Oblivion', 56)
+
+movies =[movie_1,movie_2,movie_3_,movie_4]
+i =1 
+
+for movie in movies:
+	print movies[i][1] + " is rated " + movies[i][2] + "out of 100"
+	i++
+	
+
